5b4096cad06a200f496b934b6ffcbb3b6359d47a
yyklimenko <yyklimenko@gmail.com>
2022-11-07 22:00:18 +0600

    added: type checking added into TypedObjectMeta

--- TypedObjectMeta.py
+++ TypedObjectMeta.py
@@ -5,6 +5,22 @@ from typing import Any
 
 class TypedObjectMeta(type):
 
+    @staticmethod
+    def check_type(function):
+        def wrapper(*args, **kwargs):
+            count = 1
+            for variable in function.__annotations__:
+                if variable in locals()['kwargs']:
+                    if type(locals()['kwargs'][variable]) != function.__annotations__[variable]:
+                        raise TypeError(f'Type {variable} is not correct')
+                else:
+                    if type(locals()['args'][count]) != function.__annotations__[variable]:
+                        raise TypeError(f'Type {variable} is not correct')
+                    count += 1
+            return function(*args, **kwargs)
+
+        return wrapper
+
     @classmethod
     def _create_attrs(mcs, dct):
         methods = set()
@@ -44,6 +60,8 @@ class TypedObjectMeta(type):
         if name != 'TypedObject':
             methods, variables = mcs._create_attrs(dct)
             if mcs._validate_methods(methods) and mcs._validate_variables(dct, variables):
+                for method in methods:
+                    dct[method.__name__] = TypedObjectMeta.check_type(method)
                 return super().__new__(mcs, name, bases, dct)
         else:
             return super().__new__(mcs, name, bases, dct)
